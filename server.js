4b53f73ce4ba2fbda7b74f8d66fd9f2b57ebfca4
Ghaith Shan <shang@oregonstate.edu>
2020-06-09 18:32:56 -0700

    Update server.js

--- server.js
+++ server.js
@@ -152,7 +152,6 @@ app.post('/join/addMember', function (req, res, next) {
     // var Release_Date = req.body.year;
     // var mysql = req.app.get('mysql');
     var sql = "INSERT INTO Games (Game_Name, Main_Team, Release_Date) VALUES (?,?,?)";
-    var sql2= "DELETE FROM Games WHERE Game_Name= 'Rocket League'";
     var inserts = [req.body.name, req.body.mainteam, req.body.year];
     sql = mysql.pool.query(sql,inserts,function(error, results, fields){
       if(error){
@@ -207,7 +206,7 @@ app.post('/people/acceptDelete', function (req, res, next) {
 });
 
 app.post('/people/acceptUpdate', function (req, res, next) {
-  if (req.body && req.body.name ) {
+  if (req.body && req.body.name && req.body.username && req.body.mainteam && req.body.year && req.body.game && req.body.gameid && req.body.playerId) {
   //  var collection = db.collection('players');
     var player = {
       name: req.body.name,
@@ -229,9 +228,9 @@ app.post('/people/acceptUpdate', function (req, res, next) {
     // var Main_Team = req.body.mainteam;
     // var Release_Date = req.body.year;
     // var mysql = req.app.get('mysql');
-//    var sql = "DELETE FROM GAMES WHERE Game_Name= ?;";
-   var sql = "UPDATE Games SET Main_Team= ?, SET Release_Date=? WHERE Game_Name= ? ";
-    var inserts = [ req.body.mainteam, req.body.year,req.body.name];
+    var sql = "INSERT INTO Games (Game_Name, Main_Team, Release_Date) VALUES (?,?,?)";
+    //var sql2= "DELETE FROM Games WHERE Game_Name= 'Rocket League'";
+    var inserts = [req.body.name, req.body.mainteam, req.body.year];
     sql = mysql.pool.query(sql,inserts,function(error, results, fields){
       if(error){
           console.log(JSON.stringify(error))
@@ -242,7 +241,7 @@ app.post('/people/acceptUpdate', function (req, res, next) {
           // res.redirect('/');
       }
     });
-  };
+  }
 });
 
 app.delete('/resetPage', function (req, res, next) {
